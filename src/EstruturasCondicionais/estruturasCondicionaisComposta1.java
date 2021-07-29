59f905d9b309c31a2a4f66e628cd3417af7ada6f
Thiago Silva <thisilva.sp@gmail.com>
2021-07-29 11:38:03 -0300

    Estruturas condicionais composta if e else

---
+++ src/EstruturasCondicionais/estruturasCondicionaisComposta1.java
@@ -0,0 +1,23 @@
+package EstruturasCondicionais;
+
+import java.util.Scanner;
+
+public class estruturasCondicionaisComposta1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int hora = sc.nextInt();
+
+		if (hora < 12) {
+			System.out.println("Bom dia");
+		} else {
+			System.out.println("Boa tarde");
+		}
+
+		sc.close();
+
+	}
+
+}
