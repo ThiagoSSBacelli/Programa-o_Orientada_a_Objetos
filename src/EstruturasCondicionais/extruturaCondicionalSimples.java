71507421a383fd8f3a6e54c8db85ebf6e3cd78e7
Thiago Silva <thisilva.sp@gmail.com>
2021-07-29 11:32:04 -0300

    Estruturas condicionais simples

---
+++ src/EstruturasCondicionais/extruturaCondicionalSimples.java
@@ -0,0 +1,18 @@
+package EstruturasCondicionais;
+
+
+public class extruturaCondicionalSimples {
+
+	public static void main(String[] args) {
+		
+		int x = 6;
+		
+		if(x == 4) {
+			System.out.println("Bom dia");
+		} //Neste caso nao sera impresso o BOM DIA pois X nao é igual a 4.
+				
+		System.out.println("Boa tarde");
+		System.out.println("Boa noite");	
+	}
+
+}
