c9ffd594aa589570f4b62c9d1165a7858c8ed506
Thiago Silva <thisilva.sp@gmail.com>
2021-08-10 17:58:28 -0300

    Exemplo de exercicio com e sem POO

--- src/POO/ProblemaTriangulo/MedidaDeDoisTriangulosComPoo.java
+++ src/POO/ProblemaTriangulo/MedidaDeDoisTriangulosComPoo.java
@@ -3,6 +3,8 @@ package ProblemaTriangulo;
 import java.util.Locale;
 import java.util.Scanner;
 
+import Application.FuncaoTrianglePOO;
+
 public class MedidaDeDoisTriangulosComPoo {
 
 	public static void main(String[] args) {
@@ -10,21 +12,28 @@ public class MedidaDeDoisTriangulosComPoo {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		double xA, xB, xC, yA, yB, yC;
+		FuncaoTrianglePOO x, y;
+		// Veja que a Classe FuncaoTrianglePOO foi importada e por esse motivo nao
+		// precisamos de todos aqueles atributos, assim como vimos no mesmo exercicio
+		// porem sem o POO
+
+		x = new FuncaoTrianglePOO(); //instaciando a classe
+		y = new FuncaoTrianglePOO(); //instaciando a classe
+
 		System.out.println("Enter the measures of triangle X: ");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		System.out.println("Enter the measures of triangle Y: ");
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 
-		double p = (xA + xB + xC) / 2.0;
-		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+		double p = (x.a + x.b + x.c) / 2.0;
+		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 
-		p = (yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+		p = (y.a + y.b + y.c) / 2.0;
+		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
 
